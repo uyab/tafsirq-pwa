a3de1d0e195c11f1e86c1bf492b8cc87abf9ffd0
Bayu Hendra Winata <bayu.hendra@javan.co.id>
2019-04-08 10:21:58 +0700

    Update cache list

--- service-worker.js
+++ service-worker.js
@@ -11,6 +11,15 @@ var urlsToCache = [
   "/js/nav.js",
   "/img/logo-tafsirq.png",
   "/img/banner.jpg",
+  "/img/doa-orang-tua.jpg",
+  "icon-72x72.png",
+  "icon-96x96.png",
+  "icon-128x128.png",
+  "icon-144x144.png",
+  "icon-152x152.png",
+  "icon-192x192.png",
+  "icon-384x384.png",
+  "icon-512x512.png"
 ];
 
 self.addEventListener("install", function (event) {
